d8a6d90bf6d6f2944eb9fed42dafb51bd22bf501
Calvin Lowe <calvin@calvinlowe.com>
2019-04-29 15:14:50 +1000

    Refactored app.js
    - Changed app.js so that model, view and controller and constructed when the the initialiseApp() function is called.
    - Initialise app is called on page load

--- App/app.js
+++ App/app.js
@@ -1,29 +1,28 @@
 import Model from './model.js';
 import View from './view.js';
 import Controller from './controller.js';
+import {attach} from './helper.js';
 
 /**
  * Inialises the app by creating instances of the Model, View and Controller classes.
  */
+function initialiseApp() {
+	console.log('Initialising application...');
+	/**
+	 * @type {Model}
+	 */
+	const model = new Model();
 
+	/**
+	 * @type {View}
+	 */
+	const view = new View();
 
-/**
- * @type {Model}
- */
-const model = new Model();
+	/**
+	 * @type {Controller}
+	 */
+	const controller = new Controller(model, view);
+}
 
-/**
- * @type {View}
- */
-const view = new View();
-
-/**
- * @type {Controller}
- */
-const controller = new Controller(model, view);
-
-// TODO: Add alias for add event listener to TODO MVC
-//const setView = () => controller.setView();
-
-//window.addEventListener('load', setView);
-//window.addEventListener('hashchange', setView);
+// Initialise application on window load... TODO: Should this be on first load only?
+attach(window, 'load', initialiseApp);
