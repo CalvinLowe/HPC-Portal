cb9f69113067780aa375f85770ef415909a058dd
Calvin Lowe <calvin@calvinlowe.com>
2019-05-07 18:25:38 +1000

    Getters and events
    - Added getters for job submission form and form data
    - Added event attacher for submit event

--- App/View.js
+++ App/View.js
@@ -12,17 +12,30 @@ export default class View {
 	 */
 	constructor() {
 		console.log("Constructing new instance of View...");
-		this.jobSubmissionForm = select("jobSubmissionForm");
-		this.formData = new FormData(this.jobSubmissionForm);
+		this._jobSubmissionForm = select("jobSubmissionForm");
+		//this._formData = new FormData(this._jobSubmissionForm); // TODO: not sure if this should be in view or not..
 	}
 
-	/*TODO: Do this in controller? for(var pair of formData.entries()) {
-		console.log(pair[0]+ ', '+ pair[1]);
-	} */
+	// TODO: Comments
+	get jobSubmissionForm() {
+		return this._jobSubmissionForm;
+	}
 
-	//attachEvent
+	// TODO: comments
+	get formData() {
+		return new FormData(jobSubmissionForm);
+	}
+
+	// TODO: Comments attach Event for job submission form
+	attachFormSubmitEvent(handler) {
+		let target = jobSubmissionForm;
+		let type = 'submit';
+		attach(target, type, handler);
+	}
+
 
 	// TODO: Just get the formData from the form. Figure out validation as we go along..
 	// TODO: Adding classes during editing, verfication, validation, adding a green tick once verified, a red cross is needing changing..
 	// Should verify as editing so I will need to get the selectors for each
+	// TODO: get defaults THROUGH controller from Model and attach to value on pages..
 }
