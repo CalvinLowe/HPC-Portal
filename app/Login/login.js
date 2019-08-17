0dc41711e5d376c3d0c938fbff23f26b532ebbb9
Calvin Lowe <calvin@calvinlowe.com>
2019-08-17 12:53:23 +1000

    Fixing log in event

--- app/Login/login.js
+++ app/Login/login.js
@@ -1,36 +1,78 @@
-let siteLogin = document.getElementById('login');
-let siteLogout = document.getElementById('logout');
+let isLoggedIn = false;
 
-let loginUrl = siteLogin.getAttribute('href');
+let login = document.getElementById('login');
+let logout = document.getElementById('logout');
 
-attach(siteLogin, 'click', handleLogin);
-attach(siteLogout, 'click', handleLogout);
+attach(document.getElementById('login'), 'click', handleLoginButtonClick);
+attach(document.getElementById('logout'), 'click', handleLogoutButtonClick);
 
-function handleLogin(e) {
-	//window.location = "dashboard/dashboard.html"
-	let answer = isLoggedIn();
-	//console.log(answer);
-	if (answer == true) {
-		toggleLoginShow();
+function handleLoginButtonClick(e) {
+	loginRequest();
+}
+
+// Need to wait for page https://hpcportal.rcc.uq.edu.au/client/login?service=hpcportal to return 
+// "Login complete! You can close me now :)"
+var TEST = new XMLHttpRequest();
+TEST.onload = function(){
+	if (TEST.status != 200)
+	{
+		console.log("Not yet logged in");
+		console.log(TEST.response);
+		console.log(TEST.status);
+	}
+	else {
+		console.log("Should be logged in now");
+		console.log(TEST.response);
+		console.log(TEST.status);
+	}
+};
+TEST.open('GET', 'https://hpcportal.rcc.uq.edu.au/client/login?service=hpcportal');
+TEST.send();
+
+function loginRequest() {
+	var xhr = new XMLHttpRequest();
+	xhr.onload = handleXHROnload;
+	xhr.open('GET', 'https://hpcportal.rcc.uq.edu.au/client/api/session_info');
+	xhr.responseType = 'json';
+	xhr.send();
+}
+
+function handleXHROnload() {
+	if (this.status >= 200 && this.status < 300) {
+		console.log('The request succeeded.');
+		isLoggedIn = (this.response.has_oauth_access_token == "true");
+		doUserLogin();
+	} else {
+		console.log('The request failed.');
 	}
 }
 
-function handleLogout(e) {
+function doUserLogin() {
+	if (isLoggedIn == true) {
+		console.log("Login was true");
+		console.log(isLoggedIn);
+		toggleLoginShow();
+		// TODO: set application state to logged in
+		
+		// Redirect after small timeout..
+		setTimeout(function() {
+			window.location = "Dashboard/dashboard.html"
+		}, 2000);
+		
+	}
+	else {
+		console.log("Login wasn't true");
+		console.log(isLoggedIn);
+	}
+}
+
+function handleLogoutButtonClick(e) {
 	// Check if logged out then do the rest and the redirect
 	toggleLoginShow();
 }
 
-function toggleLoginShow(element) {
-	siteLogin.classList.toggle("login--hidden");
-	siteLogout.classList.toggle("logout--hidden");
-}
-
-async function isLoggedIn() {
-	let resource = "https://hpcportal.rcc.uq.edu.au/client/api/session_info";
-	let response = await fetch(resource, {});
-	//console.log(response);
-	let responseJSON = await response.json();
-	//console.log(responseJSON);
-	// TODO: errors?
-	return responseJSON.has_oauth_access_token;
+function toggleLoginShow() {
+	console.log("toggleLoginShow called");
+	login.classList.toggle("login--hidden");
+	logout.classList.toggle("logout--hidden");
 }
