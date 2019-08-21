7c4a7f4df3fb29b32bb0d719090a077b89e4944d
Calvin Lowe <calvin@calvinlowe.com>
2019-08-21 22:27:01 +1000

    Working on listfiles

--- app/listfiles/listFiles.js
+++ app/listfiles/listFiles.js
@@ -4,23 +4,22 @@ let listFilesButton = document.getElementById("listFilesButton");
 attach(listFilesButton, 'click', handleListFilesButtonClick);
 
 function handleListFilesButtonClick() {
-	listFiles();
-	//console.log(listFiles());
+	getFileData();
 }
 
-function listFiles() {
-	//requires access_token
-	let accessToken = getAccessToken();
-	let url = 'https://hpcportal.rcc.uq.edu.au/hpcbackend/api/execute/listfolderbase64?folderpath=__based64_url__&access_token={accessToken}'
-	let response = await fetch('url');
+async function getFileData() {
+	let accessToken = await accessTokenRequest();
+	let url = `https://hpcportal.rcc.uq.edu.au/hpcbackend/api/execute/listfolderbase64?folderpath=__based64_url__&access_token=${accessToken}`;
+	let response = await fetch(url);
 	let data = await response.json();
 	console.log("listFiles data:", data);
 	//return data;
 }
 
-function getAccessToken() {
+async function accessTokenRequest() {
 	let response = await fetch('https://hpcportal.rcc.uq.edu.au/client/api/access_token');
 	let data = await response.json();
-	console.log("getAccessToken data:", data);
-	//return data;
+	if (data !== null) {
+		return data.access_token;
+	}
 }
