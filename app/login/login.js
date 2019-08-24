dd8bf8d95248818b15df19c2947693b99ce7660a
Calvin Lowe <calvin@calvinlowe.com>
2019-08-24 15:36:43 +1000

    Redirect if not logged in and not on home page

--- app/login/login.js
+++ app/login/login.js
@@ -4,29 +4,21 @@ let interval;
 let homePageLocationPath = "/";  // TODO: move to a navigation somewhere
 let dashboardLocationPath = "/dashboard"; // TODO: move to a navigation somewhere
 
-
 document.onload = loginUI();
-
-//function isLoggedIn
+// TODO: document.onload = function() {// series of function calls go here}
+//function isLoggedIn TODO:
 
 function loginUI() {
 	console.log("Log in UI called!");
 	initSessionsStorage();
 
-	console.log("Sessions storage: ", sessionStorage.getItem("isLoggedIn"));
-	console.log("window.location.pathname: ", window.location.pathname);
 	if (sessionStorage.getItem("isLoggedIn") == "false" && window.location.pathname != homePageLocationPath) {
-		console.log("window.location.pathname: ", window.location.pathname);
-		console.log("Both is logged in was false and the location path was not home");
 		console.log("Redirecting...");
 		setTimeout(function() {
-			//window.location.pathname = homePageLocationPath;
+			window.location.pathname = homePageLocationPath;
 		}, 1000);
 	} else if(sessionStorage.getItem("isLoggedIn") == "true") {
-		console.log("is logged in was true.");
 		document.body.classList.add("logged-in");
-	} else {
-		console.log("neither condition met"); // TODO:
 	}
 }
 
