8f56ce2827edfeb943c64db2c6caf73d4f09c377
Calvin Lowe <calvin@calvinlowe.com>
2019-10-04 12:45:21 +1000

    Started converting job submission for to Vue ViewModel

--- app/ViewModels/CreateJobViewModel.js
+++ app/ViewModels/CreateJobViewModel.js
@@ -1,3 +1,14 @@
 let createJobVM = new Vue({
-	el: '.job-submission-form',
-})
+	el: '.form__container',
+	data: {
+		form: {
+			jobName: 'Default job name',
+			accountGroups: ['UQ-RCC', 'qris-uq'],
+			walltime: 1,
+			select: 1,
+			ncpus: 1,
+			memory: 1,
+			payload: 'Default payload'
+		}
+	}
+});
