33117a6e25db0d7143b6a6d865ac4d3ee36a0bfe
Calvin Lowe <calvin@calvinlowe.com>
2019-10-06 21:25:27 +1000

    Updated CJVM

--- app/ViewModels/CreateJobViewModel.js
+++ app/ViewModels/CreateJobViewModel.js
@@ -1,15 +1,22 @@
+import User from '../user/User.js';
+
 let createJobVM = new Vue({
 	el: '.form__container',
 	data: {
 		form: {
-			jobName: 'Default job name',
+			jobName: '',
+			workDirectory: '',
 			selectedAccountGroup: '',
-			accountGroups: ['UQ-RCC', 'qris-uq'],
-			walltime: 1,
-			select: 1,
-			ncpus: 1,
-			memory: 1,
-			payload: 'Default payload'
-		}
+			accountGroups: '',
+			walltime: '',
+			select: '',
+			ncpus: '',
+			memory: '',
+			payload: ''
+		},
+	},
+	async created() {
+		this.form.accountGroups = await User.requestUserGroups();
 	}
 });
+
