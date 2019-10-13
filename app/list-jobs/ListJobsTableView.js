4e48f32be8282524c30c87960ad3fd59dcef0648
Calvin Lowe <calvin@calvinlowe.com>
2019-10-13 20:35:06 +1000

    Style job listing

--- app/list-jobs/ListJobsTableView.js
+++ app/list-jobs/ListJobsTableView.js
@@ -4,31 +4,25 @@ export default class ListJobsViewTable {
 		<table class="list-jobs__table">
 			<thead>
 				<tr class="job-list__job-attributes">
-					<th>Job ID</th>
-					<th>Job ID Number</th>
-					<th>Requested Time</th>
-					<th>Session ID</th>
-					<th>Requested Memory</th>
 					<th>State</th>
-					<th>Elapsed Time</th>
-					<th>Queue</th>
+					<th>Job ID Number</th>
 					<th>Job Name</th>
-					<th>Username</th>
+					<th>HPC Cluster</th>
+					<th>Requested Time</th>
+					<th>Requested Memory</th>
+					<th>Elapsed Time</th>
 				</tr>
 			</thead>
 			<tbody>
 				${jobList.map(job => 
 					`<tr>
-						<td>${job.jobID}</td>
+						<td class="${job.state == "R" ? `job-running` : 'job-queued'}">${job.getState()}</td>
 						<td>${job.jobIDNumber}</td>
-						<td>${job.requestedTime}</td>
-						<td>${job.sessionID}</td>
-						<td>${job.requestedMemory}</td>
-						<td>${job.state}</td>
-						<td>${job.elapsedTime}</td>
-						<td>${job.queue}</td>
 						<td>${job.jobName}</td>
-						<td>${job.userName}</td>
+						<td>${job.HPCCluster}</td>
+						<td>${job.requestedTime}</td>
+						<td>${job.requestedMemory}</td>
+						<td>${job.elapsedTime}</td>
 					</tr>`
 				).join('')}
 			</tbody>
